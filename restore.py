eea3dd412eb405177e6ec81d5ab2d2e13ddb94f5
nishanthreddy1432 <nishanthreddy1432@gmail.com>
2024-02-01 12:38:37 -0500

    Update restore.py

--- restore.py
+++ restore.py
@@ -33,7 +33,7 @@ class S3:
 
     def downloadFile(self, src, dest):
         try:
-            dest = "cognito-backup" + dest
+            src = "cognito-backup" + src
             bsess.resource('s3').meta.client.download_file(self.BUCKET, src, dest)
         except Exception as e:
             Logs.critical("Error downloading file")
